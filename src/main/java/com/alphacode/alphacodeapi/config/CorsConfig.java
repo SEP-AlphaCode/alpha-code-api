13454ac3e60d47d2140cc99893258c6b5d447cf0
KhanhDCQ <khanhdcq@fpt.com>
2025-08-12 18:02:54 +0700

    Update CorsConfig.java

--- src/main/java/com/alphacode/alphacodeapi/config/CorsConfig.java
+++ src/main/java/com/alphacode/alphacodeapi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:3000", "http://alpha-code.site")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
