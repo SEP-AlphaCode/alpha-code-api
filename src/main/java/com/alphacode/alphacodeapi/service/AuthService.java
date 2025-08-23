9d580d30306d255a0d34f7685be0a6e85f10ee08
Võ Huy Hoàng <145216389+ferr2764@users.noreply.github.com>
2025-08-23 22:01:16 +0700

    api login gg

--- src/main/java/com/alphacode/alphacodeapi/service/AuthService.java
+++ src/main/java/com/alphacode/alphacodeapi/service/AuthService.java
@@ -4,4 +4,6 @@ import com.alphacode.alphacodeapi.dto.LoginDto;
 
 public interface AuthService {
     LoginDto.LoginResponse login(LoginDto.LoginRequest loginRequest);
+    LoginDto.LoginResponse googleLogin(String request);
+
 }
