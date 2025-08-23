498d23feb593d80c6499aa97833bb0601aea8ec2
Võ Huy Hoàng <145216389+ferr2764@users.noreply.github.com>
2025-08-23 13:25:16 +0700

    config sưagger

--- src/main/java/com/alphacode/alphacodeapi/config/OpenAPIConfig.java
+++ src/main/java/com/alphacode/alphacodeapi/config/OpenAPIConfig.java
@@ -4,15 +4,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Configuration
 public class OpenAPIConfig {
+
     @Bean
     public OpenAPI openAPI(@Value("${open.api.title}") String title,
                            @Value("${open.api.version}") String version,
@@ -23,17 +27,8 @@ public class OpenAPIConfig {
                         .description(description)
                         .license(new License().name("Apache 2.0").url("http://springdoc.org")))
                 .servers(List.of(new Server().url(serverUrl)));
-        //                .components(
-//                        new Components()
-//                                .addSecuritySchemes(
-//                                "bearerAuth",
-//                                new SecurityScheme()
-//                                        .type(SecurityScheme.Type.HTTP)
-//                                        .scheme("bearer")
-//                                        .bearerFormat("JWT")))
-//                .security(List.of(new SecurityRequirement().addList("bearerAuth")));
-
     }
+
     @Bean
     public GroupedOpenApi groupedOpenApi() {
         return GroupedOpenApi.builder()
