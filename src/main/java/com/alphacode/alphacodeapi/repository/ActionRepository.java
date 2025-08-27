1397a0e1def046bfc86dbe0991250f1cbb8f5e95
mineduck1608 <ducdm160804@gmail.com>
2025-08-27 10:59:34 +0700

    update search for action

--- src/main/java/com/alphacode/alphacodeapi/repository/ActionRepository.java
+++ src/main/java/com/alphacode/alphacodeapi/repository/ActionRepository.java
@@ -4,11 +4,14 @@ import com.alphacode.alphacodeapi.entity.Action;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface ActionRepository extends JpaRepository<Action, UUID> {
-    Page<Action> findAllByStatus(Integer status, Pageable pageable);
+    @Query("SELECT a FROM Action a WHERE LOWER(a.name) LIKE LOWER(CONCAT('%', :searchTerm, '%')) OR LOWER(a.description) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+    Page<Action> findAllByNameOrDescriptionContaining(@Param("searchTerm") String searchTerm, Pageable pageable);
 }
