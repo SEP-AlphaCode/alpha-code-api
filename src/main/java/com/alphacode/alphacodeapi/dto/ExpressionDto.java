630c7350bee8ddeb58eff87f9291bbb943820b7c
KhanhDCQ <khanhdcq@fpt.com>
2025-08-27 14:25:18 +0700

    expression

---
+++ src/main/java/com/alphacode/alphacodeapi/dto/ExpressionDto.java
@@ -0,0 +1,35 @@
+package com.alphacode.alphacodeapi.dto;
+
+import com.alphacode.alphacodeapi.enums.AccountEnum;
+import com.alphacode.alphacodeapi.enums.ExpressionEnum;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpressionDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private UUID id;
+
+    private String name;
+    private String imageUrl;
+    private String description;
+    private Integer status;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime createdDate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime lastUpdate;
+
+    @JsonProperty("statusText")
+    public String getStatusText() {
+        return ExpressionEnum.fromCode(this.status);
+    }
+}
