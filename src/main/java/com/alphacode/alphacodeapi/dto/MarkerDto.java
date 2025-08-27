6187079ffbc6e9e9af7f9dcb4aa3477dacc36151
Võ Huy Hoàng <145216389+ferr2764@users.noreply.github.com>
2025-08-27 10:29:06 +0700

    api and entity for marker

--- src/main/java/com/alphacode/alphacodeapi/dto/MarkerDto.java
+++ src/main/java/com/alphacode/alphacodeapi/dto/MarkerDto.java
@@ -1,7 +1,11 @@
 package com.alphacode.alphacodeapi.dto;
 
+import com.alphacode.alphacodeapi.entity.Activity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,22 +16,13 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AccountDto {
+public class MarkerDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private UUID id;
 
-    private String username;
+    private String name;
 
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    private String password;
-
-    private String fullName;
-
-    private String phone;
-
-    private String email;
-
-    private Integer gender;
+    private Integer status;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime createdDate;
@@ -35,17 +30,9 @@ public class AccountDto {
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime lastEdited;
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private Integer status;
+    private UUID activityId;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private String image;
+    private String activityName;
 
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private String bannedReason;
-
-    private UUID roleId;
-
-    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private String roleName;
 }
---
+++ src/main/java/com/alphacode/alphacodeapi/entity/Marker.java
@@ -0,0 +1,49 @@
+package com.alphacode.alphacodeapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "markers")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Marker {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(
+            name = "UUID",
+            strategy = "org.hibernate.id.UUIDGenerator"
+    )
+    @Column(name = "id", updatable = false, nullable = false, columnDefinition = "uuid")
+    private UUID id;
+
+    @Column(name = "name", nullable = false, length = 255)
+    private String name;
+
+    @Column(name = "status", nullable = false)
+    private Integer status;
+
+    @Column(name = "created_date", nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "last_edited")
+    private LocalDateTime lastEdited;
+
+    @Column(name = "activity_id", nullable = false, columnDefinition = "uuid")
+    private UUID activityId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id", nullable = false, insertable = false, updatable = false)
+    private Activity activity;
+
+}
