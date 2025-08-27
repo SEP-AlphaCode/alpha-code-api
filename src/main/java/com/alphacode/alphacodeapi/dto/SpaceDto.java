f3fdd2af535a1d3e4aa9c956c2ed858d32da3338
KhanhDCQ <khanhdcq@fpt.com>
2025-08-27 14:40:16 +0700

    update

---
+++ src/main/java/com/alphacode/alphacodeapi/dto/SpaceDto.java
@@ -0,0 +1,37 @@
+package com.alphacode.alphacodeapi.dto;
+
+import com.alphacode.alphacodeapi.enums.AccountEnum;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SpaceDto {
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private UUID id;
+
+    private String name;
+    private String description;
+    private UUID organizationId;
+    private Integer status;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime createdDate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private LocalDateTime lastUpdate;
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private String organizationName; // lấy từ entity Organization
+
+    @JsonProperty("statusText")
+    public String getStatusText() {
+        return AccountEnum.fromCode(this.status);
+    }
+}
---
+++ src/main/java/com/alphacode/alphacodeapi/enums/SpaceEnum.java
@@ -0,0 +1,24 @@
+package com.alphacode.alphacodeapi.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SpaceEnum {
+    DELETED(0, "DELETED"),
+    ACTIVE(1, "ACTIVE");
+
+    private final int code;
+    private final String description;
+
+    public static String fromCode(Integer code) {
+        if (code == null) return null;
+        for (SpaceEnum s : values()) {
+            if (s.code == code) {
+                return s.description;
+            }
+        }
+        return "UNDEFINED";
+    }
+}
