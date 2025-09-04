6cdbb60757cb59ff6bbb2f574ea3174a342cbdff
Võ Huy Hoàng <145216389+ferr2764@users.noreply.github.com>
2025-09-04 16:31:20 +0700

    ok

--- src/main/java/com/alphacode/alphacodeapi/entity/Activity.java
+++ src/main/java/com/alphacode/alphacodeapi/entity/Activity.java
@@ -2,15 +2,7 @@ package com.alphacode.alphacodeapi.entity;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import io.hypersistence.utils.hibernate.type.json.JsonType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
