4995546da7cdbb530e6af998cd118edfe41dae07
mineduck1608 <ducdm160804@gmail.com>
2025-09-08 13:01:25 +0700

    Update CorsConfig.java

--- src/main/java/com/alphacode/alphacodeapi/config/CorsConfig.java
+++ src/main/java/com/alphacode/alphacodeapi/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("https://docs.alpha-code.site")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                         .allowedHeaders("*")
                         .allowCredentials(true);
